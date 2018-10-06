e0ccce847b86ef433cb6f992e3654d27d827084b
eddie <appositum@protonmail.com>
2018-10-06 19:15:08 -0300

    Simpler solution space age

--- haskell/space-age/src/SpaceAge.hs
+++ haskell/space-age/src/SpaceAge.hs
@@ -12,12 +12,15 @@ data Planet = Mercury
 secToYears :: Fractional a => a -> a
 secToYears sec = sec / 31557600
 
+period :: Planet -> Float
+period Mercury = 0.2408467
+period Venus   = 0.61519726
+period Earth   = 1
+period Mars    = 1.8808158
+period Jupiter = 11.862615
+period Saturn  = 29.447498
+period Uranus  = 84.016846
+period Neptune = 164.79132
+
 ageOn :: Planet -> Float -> Float
-ageOn Mercury age = secToYears age / 0.2408467
-ageOn Venus   age = secToYears age / 0.61519726
-ageOn Earth   age = secToYears age
-ageOn Mars    age = secToYears age / 1.8808158
-ageOn Jupiter age = secToYears age / 11.862615
-ageOn Saturn  age = secToYears age / 29.447498
-ageOn Uranus  age = secToYears age / 84.016846
-ageOn Neptune age = secToYears age / 164.79132
+ageOn planet age = secToYears age / period planet
